303a2e40ef3dad789ff2268d5bfe094ddcd373bb
amao1016 <t520u3u4@gmail.com>
2024-09-20 15:37:05 +0800

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    path('admin/', admin.site.urls),
+]
